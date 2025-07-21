26427ce68ceed4ab940e97dda3d140118e6a4657
Staduri-16 <sirichandanataduri16@gmail.com>
2025-07-21 23:01:13 +0530

    Add Playwright GitHub Actions CI

---
+++ tests/playwright.config.js
@@ -0,0 +1,12 @@
+// playwright.config.js
+const { defineConfig } = require('@playwright/test');
+
+module.exports = defineConfig({
+  timeout: 60000,
+  retries: 1,
+  reporter: [['html', { open: 'never' }]], // This enables HTML report
+  use: {
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+  },
+});
